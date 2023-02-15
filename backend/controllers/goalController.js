5210fb5ad3eab314afa2f062d5b50490e0811b80
james kiongo <jameskiongo@jamess-MacBook-Air.local>
2023-02-15 12:57:56 +0300

    finished mern api

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,12 @@
 const asyncHandler = require('express-async-handler');
 
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 //@desc get all goals
 // @route get /api/goals
 //@access private
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id })
     res.status(200).json(goals)
 })
 //@desc set a goals
@@ -17,7 +18,8 @@ const setGoal = asyncHandler(async (req, res) => {
         throw new Error('Please add data')
     }
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
     res.status(200).json(goal)
 })
@@ -29,6 +31,18 @@ const updateGoals = asyncHandler(async (req, res) => {
     if (!goal) {
         res.status(400)
         throw new Error('Goal not found')
+    }
+    const user = await User.findById(req.user.id)
+
+    //check for user
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+    //confirm logged in user matches goals user
+    if (goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
     }
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true })
     res.status(200).json(updatedGoal)
@@ -42,7 +56,19 @@ const deleteGoals = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
-    await goal.remove
+    const user = await User.findById(req.user.id)
+
+    //check for user
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+    //confirm logged in user matches goals user
+    if (goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+    await goal.remove()
     res.status(200).json({ id: req.params.id })
 })
 module.exports = {
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,8 +2,10 @@ const express = require("express")
 const router = express.Router();
 const { getGoals, setGoal, deleteGoals, updateGoals } = require('../controllers/goalController')
 
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').delete(deleteGoals).put(updateGoals)
+const { protect } = require('../middleware/authMiddleware')
+
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoals).put(protect, updateGoals)
 
 
 module.exports = router
